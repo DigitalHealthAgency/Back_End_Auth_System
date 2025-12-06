ce4a77f87c0f59dbfa58d340cddb8c0f82e9752c
icp-smartprojects <icp.smartprojects@gmail.com>
2025-12-06 12:59:06 +0300

    Fix validation error messages

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,8 +1,10 @@
 module.exports = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body, { abortEarly: false, stripUnknown: true });
   if (error) {
+    const firstError = error.details[0];
     return res.status(400).json({
-      message: 'Validation error',
+      message: firstError.message,
+      code: firstError.type.toUpperCase().replace(/\./g, '_'),
       details: error.details.map(d => ({
         message: d.message,
         path: d.path,
